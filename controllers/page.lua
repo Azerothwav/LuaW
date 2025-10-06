d476b3c09e9beecfc0b86c13989d37272f854b70
Azeroth <azerothwaav@proton.me>
2025-10-06 15:19:52 +0200

    feat: render default page on default route

--- controllers/page.lua
+++ controllers/page.lua
@@ -3,7 +3,7 @@ local parser = require('utils.parser')
 local router = require('handlers.router')
 local renderer = require('utils.renderer')
 
-router.add_route('GET', '/page', function(client, request)
+router.add_route('GET', '/', function(client, request)
    local html = renderer.render('page.html', { title = 'Page' })
 
    copas.send(client, parser.html_response(200, html))
